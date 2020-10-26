368d946ce8f4820ae166883ee755c5a5f4abd0df
iachieve <mhalim80@hotmail.com>
2020-10-26 06:55:33 -0700

    demo

--- src/dev/practice/one/ABDemo.java
+++ src/dev/practice/one/ABDemo.java
@@ -4,9 +4,17 @@ import java.util.regex.Pattern;
 
 public class ABDemo {
     public static void main(String[] args) {
+
+        // true (2nd char is r)
         System.out.println(Pattern.matches(".r", "ar"));
+
+        // false (2nd char is not r)
         System.out.println(Pattern.matches(".r", "ak"));
+
+        // false (has more than 2 char)
         System.out.println(Pattern.matches(".r", "arr"));
 
+        // true (third char is r)
+        System.out.println(Pattern.matches("..r", "par"));
     }
 }
